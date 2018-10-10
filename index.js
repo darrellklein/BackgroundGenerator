458054031d64171563513dfa9ce717e134a7dfbd
Darrell Klein <dkleined92@gmail.com>
2018-10-10 16:09:44 -0700

    working, finished for now. will play more later

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const body = document.getElementById("#gradient-background")
+const body = document.getElementById("gradient");
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 =  document.querySelector(".color2");
 
-color1.addEventListener("input", function(){
-    body.style.backgroundColor = "linear-gradient(to right, "
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right, "
     + color1.value
     + ", "
-    + color2.value + ");";    
-});
+    + color2.value 
+    + ")";
 
-color2.addEventListener("input", function(){
-    console.log(color2.value);
-});
+    css.textContent = body.style.background + ";";
+};
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
