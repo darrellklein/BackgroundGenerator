2a9860db11db57f1c0cd99c1dc528c72c8a24755
Darrell Klein <darrelldklein@gmail.com>
2018-10-10 16:14:44 -0700

    test

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 =  document.querySelector(".color2");
 
+// random comment to check something
+
 function setGradient() {
     body.style.background = 
     "linear-gradient(to right, "
